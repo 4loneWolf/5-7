7b19664dd9c5677e3133b2e5cd1a200122339380
pepega <drelkasteam@gmail.com>
2024-12-26 18:28:20 +0700

    Третья итерация (III)

--- main.py
+++ main.py
@@ -20,3 +20,30 @@ def print_result(frequency):
     print("Частотный анализ текста:")
     for char, freq in frequency.items():
         print(f"{char}: {freq:.4f}")
+
+text = None
+frequency = None
+while True:
+    print("Меню:")
+    print("1. Ввод исходных данных")
+    print("2. Выполнение алгоритма")
+    print("3. Вывод результата")
+    print("4. Завершение работы программы")
+    choice = input("Выберите пункт меню: ")
+    if choice == '1':
+        text = input_data()
+    elif choice == '2':
+        if text is not None:
+            frequency = frequency_analysis(text)
+        else:
+            print("Сначала введите данные.")
+    elif choice == '3':
+        if frequency is not None:
+            print_result(frequency)
+        else:
+            print("Сначала выполните алгоритм.")
+    elif choice == '4':
+        print("Завершение работы программы.")
+        break
+    else:
+        print("Неверный выбор. Попробуйте снова.")
