5a62d721a4a707881d24f685bb98c1fe537e3407
pepega <drelkasteam@gmail.com>
2024-12-26 18:21:50 +0700

    Вторая итерация (II)

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+import re
+
 def input_data():
-    pass
+    text = input("Введите текст: ")
+    return text
 
 def frequency_analysis(text):
-    pass
+    text = re.sub(r'[^a-zA-Z0-9]', '', text)
+    frequency = {}
+    total_chars = len(text)
+    for char in text:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    frequency_percentage = {char: count / total_chars for char, count in frequency.items()}
+    return frequency_percentage
 
 def print_result(frequency):
-    pass
+    print("Частотный анализ текста:")
+    for char, freq in frequency.items():
+        print(f"{char}: {freq:.4f}")
